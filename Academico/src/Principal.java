a0e3c0a98c09933a58e77bc6062d2a3e6c2732df
RA21437247 <RA21437247@AN0700121077358.dinfor.uniceub>
2017-08-17 21:55:39 -0300

    Commit Inicial

---
+++ Academico/src/Principal.java
@@ -0,0 +1,22 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// Variavel
+		Aluno a1 = new Aluno("Ana Luiza", "4572896", 25, "Ciencia da Computação", "vespertino", "F");
+		
+		Disciplina d1 = new Disciplina("LTP1","Ciencia da Computação", 48);
+		
+		
+	//	System.out.println("Nome: " + a1.getNome());
+	//	System.out.println("RA: " + a1.getRA());
+	//	System.out.println("Idade: " + a1.getIdade());
+	//	System.out.println("Curso: " + a1.getCurso());
+	//	System.out.println("Turno: " + a1.getTurno());
+	//	System.out.println("Sexo: " + a1.getSexo());
+		
+		System.out.println(a1.imprimirAluno());
+		
+	}
+
+}
