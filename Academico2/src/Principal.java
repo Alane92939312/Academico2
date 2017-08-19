120d0ccd6e267d15165b16d0d6f20a786cb7cc73
RA21437247 <RA21437247@AN0700121077358.dinfor.uniceub>
2017-08-18 21:29:08 -0300

    mudança de data

--- Academico2/src/Principal.java
+++ Academico2/src/Principal.java
@@ -1,22 +1,23 @@
-import java.util.Calendar;
+import java.time.LocalDate;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		Calendar dataNascimento = Calendar.getInstance();
-		dataNascimento.set(Calendar.YEAR, 2011);
-		dataNascimento.set(Calendar.MONTH, Calendar.AUGUST);
-		dataNascimento.set(Calendar.DAY_OF_MONTH, 10);	
 		
 	
 		//Criar objeto da classe Aluno
-				Aluno c1 = new Aluno("Luiz", "125478", "M", dataNascimento);		
+				Aluno a1 = new Aluno("Luiz", "125478", "M", LocalDate.of(2017,6,30));		
 				Disciplina d1 = new Disciplina("Joao", "Portugues", 45);
 				Professor p1 = new Professor("Arthur", "457", "Doutorado", "02504177899");
 				Turma t1 = new Turma("Turma K", p1, d1);
 				
-				System.out.println(c1.getDataComoString());
+				System.out.println(a1.getDataComoString());
+				
+				System.out.println(a1.getRA());
+				System.out.println(d1.getCurso());
+				System.out.println(p1.getCPF());
+				System.out.println(t1.getDisciplina());
 				
 	}
 	}
